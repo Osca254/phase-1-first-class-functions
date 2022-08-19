eba300bb4e310e304b4c3a3027f48335ef1d27cf
moringa <oscarokumu47@gmail.com>
2022-08-19 13:02:04 +0300

    modify index.js && package-lock.json

---
+++ index.js
@@ -0,0 +1,15 @@
+const { spy } = require("chai");
+
+function receivesAFunction(callback){
+   return callback ();
+}
+function returnsANamedFunction(callback){
+    let function1 = function(){
+   return callback();
+    }
+}
+function returnsAnAnonymousFunction (){
+    return function () {
+
+    }
+}
